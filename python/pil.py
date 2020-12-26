34dd7c9b3875317ddc55a8d93f5f7e9e86791ca9
Ulrich Pech <ulrichpech@gmail.com>
2020-12-26 20:22:04 +0100

    python script

---
+++ python/pil.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from PIL import Image
+import math
+
+
+class ImgTo7Color:
+    WIDTH = 600
+    HEIGHT = 448
+
+    def __init__(self):
+        self.ratio = self.HEIGHT / self.WIDTH
+        self.palette = [0, 0, 0, 255, 255, 255, 255, 128, 28, 0, 0, 255, 0, 255, 0, 255, 255, 0, 255, 0, 0]
+        self.palimage = Image.new('P', (49, 49))
+        self.palimage.putpalette(self.palette * 32)
+
+    def crop_y(self, image):
+        w, h = image.size
+        new_h = math.floor(h * self.ratio)
+        y = math.floor((h - new_h) / 2)
+        area = (0, y, w, h - y)
+
+        return image.crop(area)
+
+    def crop_x(self, image):
+        w, h = image.size
+        img_ratio = h / w
+        crop_factor = img_ratio / self.ratio
+        new_h = math.floor(w * crop_factor)
+        x = math.floor((w - new_h) / 2)
+        area = (x, 0, w - x, h)
+
+        return image.crop(area)
+
+    def resize(self, image):
+        return image.resize((self.WIDTH, self.HEIGHT), Image.ANTIALIAS)
+
+    def quantizetopalette(self, image, palette, dither=False):
+        """Convert an RGB or L mode image to use a given P image's palette.
+           from https://stackoverflow.com/questions/29433243/convert-image-to-specific-palette-using-pil-without-dithering/29438149
+        """
+
+        image.load()
+
+        # use palette from reference image
+        palette.load()
+        if palette.mode != "P":
+            raise ValueError("bad mode for palette image")
+        if image.mode != "RGB" and image.mode != "L":
+            raise ValueError(
+                "only RGB or L mode images can be quantized to a palette"
+            )
+        im = image.im.convert("P", 1 if dither else 0, palette.im)
+        # the 0 above means turn OFF dithering
+
+        # Later versions of Pillow (4.x) rename _makeself to _new
+        try:
+            return image._new(im)
+        except AttributeError:
+            return image._makeself(im)
+
+    def convert(self, image_path, dither=True):
+        image = Image.open(image_path).convert("RGB")
+        image = self.crop_x(image)
+        image = self.resize(image)
+        image = self.quantizetopalette(image, self.palimage, dither)
+        return image
+
+if __name__ == '__main__':
+    conv = ImgTo7Color()
+    image = conv.convert("falcon9_full.jpg")
+    image.show()
